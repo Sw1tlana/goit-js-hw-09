518896720e41055dc9122521d61bb8a3dc315c5d
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2023-12-26 21:26:42 +0200

    Changes in the 2-form task code

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,36 +1,35 @@
 const form = document.querySelector(".feedback-form");
-const localeStorageKey = "feedback-form-state";
+const localStorageKey = "feedback-form-state";
 
-const savedStates = localStorage.getItem(localeStorageKey);
-if(savedStates) {
+const savedStates = localStorage.getItem(localStorageKey);
+
+    if(savedStates) {
     const { email, message } = JSON.parse(savedStates);
-    form.elements.email.value = email;
-    form.elements.message.value = message;
+    form.elements.email.value = email !== undefined ? email : "";
+    form.elements.message.value = message !== undefined ? message : "";
 }
 
 form.addEventListener("input", (evt) => {
-    changeLocaleStorage()
+    changeLocaleStorage(evt);
 });
 
 form.addEventListener("submit", (evt) => {
-    submitForm();
+    submitForm(evt);
 });
 
 function changeLocaleStorage (evt) {
-    const elements = form.elements;
-    const message = elements.message.value.trim();
-    const email = elements.email.value.trim();
-    localStorage.setItem(localeStorageKey, JSON.stringify({ email, message }));
-}
-
-function submitForm (evt) {
-    evt.preventDefault();
     const elements = form.elements;
     const submitData = {
         email: elements.email.value.trim(),
         message: elements.message.value.trim()
     }
-    console.log(submitData);
-    localStorage.removeItem(localeStorageKey);
+    
+    localStorage.setItem(localStorageKey, JSON.stringify(submitData));
+}
+
+function submitForm (evt) {
+    evt.preventDefault();
+    localStorage.removeItem(localStorageKey);
     form.reset();
-};
+}
+
